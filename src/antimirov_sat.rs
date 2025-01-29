fc21f606343f2a5f0dc9268ff63dcb1c23de8293
Tiching Kao <tichingkao@gmail.com>
2025-01-28 16:26:26 -0800

    BFS sat algo

--- src/antimirov_sat.rs
+++ src/antimirov_sat.rs
@@ -1,6 +1,6 @@
 use crate::antimirov::{derivative, nullable};
 use crate::classes::{CharExpression, CharVar, GenRegex};
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 use std::rc::Rc;
 
 pub struct SatChecker {}
@@ -16,19 +16,20 @@ impl SatChecker {
         SatChecker {}
     }
     pub fn satisfiable(&mut self, expr: &Rc<GenRegex>) -> bool {
-        let mut sat_stack = vec![DerivativeDepth {
+        let mut sat_stack = VecDeque::new();
+        sat_stack.push_back(DerivativeDepth {
             gre: expr.clone(),
             depth: 0,
-        }];
+        });
         let mut visited: HashSet<Rc<GenRegex>> = HashSet::new();
-        while let Some(layer) = sat_stack.pop() {
+        while let Some(layer) = sat_stack.pop_front() {
             if !nullable(&layer.gre).is_empty() {
                 return true;
             } else {
                 let deriv = derivative(&layer.gre, &self.get_fresh_var(layer.depth));
                 for ele in deriv {
                     if !visited.contains(ele.get_expr()) {
-                        sat_stack.push(DerivativeDepth {
+                        sat_stack.push_back(DerivativeDepth {
                             gre: ele.get_expr().clone(),
                             depth: layer.depth + 1,
                         });
