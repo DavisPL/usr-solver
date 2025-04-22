9f5dc51ec9ec87d915cc5c480216cab5cc8dc9aa
Caleb Stanford <cdstanford@ucdavis.edu>
2025-04-22 16:18:01 -0700

    :warning: deprecate AB solver

--- src/solver/ab_solver.rs
+++ src/solver/ab_solver.rs
@@ -3,6 +3,10 @@
 //! Antimirov-Brzoziwski Solver (for lack of a better name) that chooses
 //! Antimirov for cases with no ITE, complement, or string indexes, and Brzozowski otherwise.
 //!
+//! ***** DEPRECATED *****
+//! ***** Pls do not use AB solver. *****
+
+#![allow(unreachable_code, unused_variables)]
 
 use super::antimirov::AntimirovSolver;
 use super::brzozowski::BrzozowskiSolver;
@@ -18,12 +22,17 @@ pub struct ABSolver {
 
 impl ABSolver {
     pub fn new() -> Self {
+        panic!("AB solver is deprecated.");
+
         Default::default()
     }
 }
 
 impl Solver for ABSolver {
     fn satisfiable(&mut self, gre: &Rc<crate::types::regex::GenRegex>) -> bool {
+
+        panic!("AB solver is deprecated.");
+
         if gre.contains_ite_complement_or_str_index() {
             self.solver_b.satisfiable(gre)
         } else {
